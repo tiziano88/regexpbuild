f328a9951ed8bc340243187c85b21de8fc17b349
Tiziano Santoro <tzn@google.com>
2014-05-08 21:54:09 +0100

    more tests

--- regexpbuild_test.go
+++ regexpbuild_test.go
@@ -5,19 +5,42 @@ import (
 )
 
 type testPair struct {
-	regexp   string
+	regexp   Builder
 	expected string
 }
 
 var tests = []testPair{
-	{MinToMax(Whitespace(), 3, 10).Build(), `\s{3,10}`},
-	{Literal("www.google.com").Build(), `www\.google\.com`},
+	{
+		MinToMax(Whitespace(), 3, 10),
+		`\s{3,10}`,
+	},
+	{
+		Sequence(
+			OneOrMore(WordCharacter()),
+			Literal("@"),
+			OneOrMore(WordCharacter()),
+			Literal("."),
+			OneOrMore(WordCharacter())),
+		`\w+\@\w+\.\w+`,
+	},
+	{
+		Literal("www.google.com"),
+		`www\.google\.com`,
+	},
+	{
+		Sequence(
+			MinToMax(Digit(), 1, 3), Literal("."),
+			MinToMax(Digit(), 1, 3), Literal("."),
+			MinToMax(Digit(), 1, 3), Literal("."),
+			MinToMax(Digit(), 1, 3)),
+		`\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}`,
+	},
 }
 
 func TestRegexp(t *testing.T) {
 	for i, pair := range tests {
-		if pair.regexp != pair.expected {
-			t.Errorf("For pair #%d expected %s got %s", i, pair.expected, pair.regexp)
+		if pair.regexp.Build() != pair.expected {
+			t.Errorf("For pair #%d expected %s got %s", i, pair.expected, pair.regexp.Build())
 		}
 	}
 }
